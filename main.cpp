a04bb2be5413f93c3d9b2d38d919107a0476b182
sheputis <arnoldas@student.matnat.uio.no>
2015-09-30 11:57:49 +0200

    still on b)

--- main.cpp
+++ main.cpp
@@ -2,20 +2,26 @@
 #include <cmath>
 //#include "armadillo"
 #include <lib.h>
+#include <algorithm>
 
 using namespace std;
 //using namespace arma;
+#include "time.h"
+
+
 
 //void Jacobi_rotation();
-void max_non_diag(double **,int); //matrix double ** A, dimention of the matrix int n
+void Jacobi_rotations(double **,int); //matrix double ** A, dimention of the matrix int n
 void matr_rotation(double **,double ,double,int, int, int);
 void print_out_matrix(double **,int);
+void find_min_eigenvalues(double **,int);
 
-void matrix_construct(double,double,int);
+void matrix_construct(double,double,int,double**);
 
 int main()
 {
-
+    clock_t start, finish; // declaring start and final time of the whole program
+    start =clock();
     int n = 3;
     double **A;
     A=new double*[n];
@@ -34,21 +40,34 @@ int main()
     //printing out the matrix
     print_out_matrix(A,n);
     //a)finding the max value of matrix:
-    max_non_diag(A,n);
+//    Jacobi_rotations(A,n);
 
     //constructing a matrix to solve Schrodinger with potential V:
+    cout<< endl<<"Quantum eigenv"<<endl;
     double ro_min=0; //domain : from ro_min to ro_max
-    double ro_max=1;
-    n=10;     // the domain will be split up in n points
-    matrix_construct(ro_min,ro_max,n);
-    cout<< endl<<"Quantum eigenv"<<"done";
+    double ro_max=3.5;
+    n=800;     // the domain will be split up in n points
+
+    double **B;
+    B=new double*[n];
+    matrix_construct(ro_min,ro_max,n,B);
 
 
+ //   print_out_matrix(B,n);
+
+    Jacobi_rotations(B,n);
+    cout <<"very excite!: "<<endl;
+
+    find_min_eigenvalues(B,n);
+    finish=clock();
+    cout<< endl<< "time that it took to run the program: "<< ((finish-start)/CLOCKS_PER_SEC)<<endl;
+    cout <<"n_step = "<<n<<endl;
     return 0;
 }
 
-void max_non_diag(double ** A_, int n_){
-    int amount_of_loops=0;
+void Jacobi_rotations(double ** A_, int n_){
+    cout<< "initializing Jacobi_rotations " << n_ <<" points"<<endl;
+    int amount_of_trans=0;
     double epsilon = pow(10,-8); //epsilon is going to be our boundary for 0
     int i_max;int j_max;
     double max;
@@ -65,9 +84,8 @@ void max_non_diag(double ** A_, int n_){
 
         }
     }
-    cout <<"maximum value :"<<max<<endl<<"i and j for max values:"<<i_max<<" , "<<j_max<<endl;
     //this line terminates the loop when the matrix is diagonalized:
-    if(epsilon>abs(max)){cout<<"max value is : "<<max;break;}
+    if(epsilon>abs(max)){/*cout<<"max value is : "<<max;*/break;}
     int k;int l;
     if(i_max>j_max){l=i_max;k=j_max;}
     else           {l=j_max;k=i_max;}
@@ -76,9 +94,8 @@ void max_non_diag(double ** A_, int n_){
     // PART 2
     //introducing tau as it is written in a), to find the angle:
     double tau;
-   // tau = ((*(*(A_+l)+l))-(*(*(A_+k)+k)))/(2*(*(*(A_+k)+l))); // a very confusing way of pointing to variables
     tau = (A_[l][l]-A_[k][k])/(2*A_[k][l]);
-    cout<< " this is tau :"<< tau<<endl;
+
     //using the quadratic formula t=-tau+-sqrt(1+tau^2) and choosing the one with least absolute value...
     //... meaning least rotation.
     double t1=-tau+sqrt(1+pow(tau,2));double t2=-tau-sqrt(1+pow(tau,2));
@@ -86,25 +103,17 @@ void max_non_diag(double ** A_, int n_){
     double t;
     if(t1>t2){ t=t1;}
     else{t=t2;}
-    cout <<"tangens , smaller, bigger: "<<t<<" "<<t1<<" "<<t2<<endl;
-    //cosine c and sine s is :
+
     double c = 1./sqrt(1+pow(t,2));
     double s = t*c;
-    cout << "cosine and sine: "<<c<<" "<<s<<endl;
-    cout << "checking if b_l_k is 0: "<<(A_[k][k]-A_[l][l])*c*s+A_[k][l]*(c*c-s*s)<<endl;
-
-
     //  PART 3 updating the matrix:
     // updating the matrix A to A' by updating values with matrix multiplication A'=StAS
-    cout<<endl<< "PART 3:"<<endl;
     matr_rotation(A_,c,s,k,l,n_);
-    print_out_matrix(A_,n_);
-    amount_of_loops+=1;
-    cout<< ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"<<endl<<"amount of loops: "<<amount_of_loops<<endl;
+    amount_of_trans+=1;
 
-    if(amount_of_loops>10){break;}
-    cout<< ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"<<endl;
-    }
+
+      }
+     cout<< "Jacobi_rotations ended, amount of transformations: "<<amount_of_trans<<endl;
 }
 
 void matr_rotation(double **A, double c,double s,int k, int l,int n){//implementing the rotation by given formulas
@@ -134,7 +143,7 @@ void matr_rotation(double **A, double c,double s,int k, int l,int n){//implement
 
 }
 void print_out_matrix(double **A,int n){
-    cout<<"printing out the matrix A: "<<endl;
+  //  cout<<"printing out the matrix: "<<endl;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             cout<<A[i][j]<<" ";
@@ -142,24 +151,27 @@ void print_out_matrix(double **A,int n){
         cout<<endl;
     }
 }
-void matrix_construct(double ro_min, double ro_max,int n){
+void matrix_construct(double ro_min, double ro_max,int n,double **A){
+    cout <<"initializing a construct of "<<n<<" point matrix A"<<endl;
     //defining step length:
     double h=(ro_max-ro_min)/n;
     double ro=ro_min;
-    double **A;
-    A=new double*[n-1];
+//    double **A;
+//    A=new double*[n-1];
 
     double d1=2./(h*h);
+
     double e1=-1/(h*h);
 
-    for(int i=0;i<n-1;i++){
-        A[i]=new double[n-1];
+    for(int i=0;i<n;i++){
+        A[i]=new double[n];
         ro=ro_min+h*i;
+      //  cout<< ro<<endl;
 
-        for(int j=0;j<n-1;j++){
+        for(int j=0;j<n;j++){
 
             if(i==j){A[i][j]=d1+ro*ro;}
-            else if(j==(i-1)&& j==(i+1)){A[i][j]=e1;}
+            else if(j==(i-1) || j==(i+1)){A[i][j]=e1;}
             else{
                 A[i][j]=0;
             }
@@ -167,5 +179,19 @@ void matrix_construct(double ro_min, double ro_max,int n){
         }
 
     }
+    cout<< "ending matrix construct"<<endl;
 
+}
+void find_min_eigenvalues(double **A,int n){
+    double *a;
+    a=new double[n];
+    double min=100;
+    for(int i=0;i<n;i++){
+        a[i]=A[i][i];
+        if(a[i]<min){min=a[i];}
+//        cout<<"very excite : "<< a[i]<<endl;
+    }
+    sort(a,a+n);
+    for(int i=0;i<3;i++){cout<< "eig value: "<< a[i]<<endl;}
+//    cout<< "finish with the min value: "<< min<<endl;
 }
